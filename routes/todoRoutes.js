c874d30cc71ef4f18d4d9bc83caaff0a7f216a64
Angelo Cataldo <angelocataldo33@gmail.com>
2023-03-23 13:37:05 -0400

    initial nav

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
-router.get("/watchlist", (req, res) => {
+router.get(`/todo/:todolist`, (req, res) => {
     res.render("blank");
 })
 
 module.exports = router;
+
--- server.js
+++ server.js
@@ -4,23 +4,26 @@ const app = express();
 app.use(layouts);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 const port = process.env.PORT || 5000;
 
 // setting up mongoose
 const mongoose = require('mongoose');
-const mongodb_URI = 'mongodb://localhost/yoangelo_website'
-//mongoose.connect(mongodb_URI, { useNewUrlParser: true });
-
-
+const mongodb_URI = 'mongodb://0.0.0.0:27017/'
+mongoose.connect(mongodb_URI, {
+    dbName: 'yoangelo_website_db',
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}).then(() => console.log("Connected to MongoDB")).catch(error => console.log(error));
 
 app.get("/", (req, res) => {
     res.render('blank');
 })
 
 // route imports
-app.use(require('./routes/watchlistRoutes'))
+app.use(require('./routes/todoRoutes'))
 
 app.listen(port, () => {
     const url = `http://localhost:${port}`;
