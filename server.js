e680b0e187ca8a6f725b618855c22513467eb353
Angelo Cataldo <angelocataldo33@gmail.com>
2023-03-23 02:20:56 -0400

    added layout

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 const express = require("express");
+const layouts = require("express-ejs-layouts");
 const app = express();
+app.use(layouts);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
 const port = process.env.PORT || 5000;
 
 // setting up mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yoangelo_website', { useNewUrlParser: true });
+const mongodb_URI = 'mongodb://localhost/yoangelo_website'
+//mongoose.connect(mongodb_URI, { useNewUrlParser: true });
 
 
 
 app.get("/", (req, res) => {
-    res.send("Home Page: unfinished");
-})
-app.get("/test", (req, res) => {
-    res.send("TEST");
+    res.render('blank');
 })
 
+// route imports
+app.use(require('./routes/watchlistRoutes'))
 
-app.listen(port, () => {console.log(`Server started on port ${port}`)})
+app.listen(port, () => {
+    const url = `http://localhost:${port}`;
+    console.log(`Listening on ${url}`);
+  });
