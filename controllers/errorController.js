57025cf84417385fdf04bbbece19fb6277f0b2b2
Angelo Cataldo <angelocataldo33@gmail.com>
2023-03-25 21:53:36 -0400

    current so pixel can help figure out css lol

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -4,7 +4,6 @@ exports.respondInternalError = (error, req, res, next) => {
 };
 
 exports.respondRouteNotFound = (req, res, next) => {
-  console.log("NOT FOUND")
   res.status(404);
   const msg = req.url + " is not an available route"
   res.render('error', {message: msg});
