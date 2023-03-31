d48373fe05affd5d871e960ac0dd632c2d2a7ee7
Angelo Cataldo <angelocataldo33@gmail.com>
2023-03-31 00:33:56 -0400

    init countdowns

--- routes/countdownRoutes.js
+++ routes/countdownRoutes.js
@@ -6,10 +6,12 @@ router.get('/', async (req, res) => {
     const user = await User.findById(req.session.userId);
     const userId = user._id;
     const partnerId = user.partnerId;
-    res.locals.todos = await Countdown.find({'creatorInfo.creatorId': {$in: [userId, partnerId]}})
+    res.locals.countdowns = await Countdown.find({'creatorInfo.creatorId': {$in: [userId, partnerId]}})
         .sort({endsAt: 1});
-    console.log(res.locals.todos);
-    res.render('countdowns');
+    res.locals.page_title = "Countdowns";
+    res.locals.addRoute = "idkyetCountdownAdd";
+    const success = req.flash('success');
+    res.render('lists/listContainer', {success, innerList: "countdowns"});
 });
 
 module.exports = router;
