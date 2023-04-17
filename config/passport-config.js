7eea73363deb822c5b8695f41059f6d95d3fd8f2
Angelo Cataldo <angelocataldo33@gmail.com>
2023-04-17 00:27:29 -0400

    callback test

--- config/passport-config.js
+++ config/passport-config.js
@@ -27,8 +27,7 @@ const setUpLocalPassport = (passport) => {
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const setUpGooglePassport = (passport) => {
-    let callbackURL = process.env.HOST_URL;
-    callbackURL += "/auth/google/callback";
+    callbackURL = "/auth/google/callback";
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
